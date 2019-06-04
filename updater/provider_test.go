982167069444f9042ce57a715a879aac518b93e1
Manuel Carmona <manu.carmona90@gmail.com>
2019-06-04 16:54:09 +0100

    updater: add update provider and scaffolding function for update repositories.
    Signed-off-by: Manuel Carmona <manu.carmona90@gmail.com>

---
+++ updater/provider_test.go
@@ -0,0 +1,134 @@
+package updater
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/src-d/gitcollector"
+	"github.com/src-d/gitcollector/library"
+	"github.com/src-d/go-borges"
+	"github.com/src-d/go-borges/plain"
+	"github.com/src-d/go-borges/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUpdatesProvider(t *testing.T) {
+	var require = require.New(t)
+	require.True(true)
+
+	ids := []borges.LocationID{
+		"a", "b", "c", "d", "e", "f",
+	}
+
+	lib := &testLib{locIDs: ids[:3]}
+
+	queue := make(chan gitcollector.Job, 30)
+	provider := NewUpdatesProvider(lib, queue, &UpdatesProviderOpts{
+		TriggerOnce:     true,
+		TriggerInterval: 500 * time.Microsecond,
+	})
+
+	go runProvider(t, provider)
+
+	time.Sleep(100 * time.Millisecond)
+	require.Len(lib.locIDs, len(queue))
+
+	provider.opts.TriggerOnce = false
+	go runProvider(t, provider)
+
+	time.Sleep(200 * time.Microsecond)
+	for _, id := range ids[3:] {
+		lib.addLocationID(id)
+	}
+
+	time.Sleep(250 * time.Millisecond)
+	require.NoError(provider.Stop())
+
+	require.Len(queue, cap(queue))
+	for i := 0; i < cap(queue); i++ {
+		job := <-queue
+		j, ok := job.(*library.Job)
+		require.True(ok)
+		require.Contains(ids, j.LocationID)
+	}
+}
+
+func runProvider(t *testing.T, provider *UpdatesProvider) {
+	t.Helper()
+	require.True(
+		t,
+		gitcollector.ErrProviderStopped.Is(provider.Start()),
+	)
+}
+
+type testLib struct {
+	mu     sync.RWMutex
+	locIDs []borges.LocationID
+}
+
+var _ borges.Library = (*testLib)(nil)
+
+func (l *testLib) addLocationID(id borges.LocationID) {
+	l.mu.Lock()
+	l.locIDs = append(l.locIDs, id)
+	l.mu.Unlock()
+}
+
+func (l *testLib) ID() borges.LibraryID { return "test" }
+
+func (l *testLib) Init(id borges.RepositoryID) (borges.Repository, error) {
+	return nil, borges.ErrNotImplemented.New()
+}
+
+func (l *testLib) Get(
+	id borges.RepositoryID,
+	mode borges.Mode,
+) (borges.Repository, error) {
+	return nil, borges.ErrNotImplemented.New()
+}
+
+func (l *testLib) GetOrInit(id borges.RepositoryID) (borges.Repository, error) {
+	return nil, borges.ErrNotImplemented.New()
+}
+
+func (l *testLib) Has(
+	id borges.RepositoryID,
+) (bool, borges.LibraryID, borges.LocationID, error) {
+	return false, "", "", borges.ErrNotImplemented.New()
+}
+
+func (l *testLib) Repositories(
+	mode borges.Mode,
+) (borges.RepositoryIterator, error) {
+	return nil, borges.ErrNotImplemented.New()
+}
+
+func (l *testLib) Location(id borges.LocationID) (borges.Location, error) {
+	return nil, borges.ErrNotImplemented.New()
+}
+
+func (l *testLib) Locations() (borges.LocationIterator, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	var locs []borges.Location
+	for _, id := range l.locIDs {
+		loc, err := plain.NewLocation(id, nil, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		locs = append(locs, loc)
+	}
+
+	return util.NewLocationIterator(locs), nil
+}
+
+func (l *testLib) Library(id borges.LibraryID) (borges.Library, error) {
+	return nil, borges.ErrNotImplemented.New()
+}
+
+func (l *testLib) Libraries() (borges.LibraryIterator, error) {
+	return nil, borges.ErrNotImplemented.New()
+}
